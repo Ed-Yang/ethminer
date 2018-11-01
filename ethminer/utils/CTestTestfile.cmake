a26324144ea4a83ac1b91379e13a6f663844d7f4
Ed-Yang <ed.yang.123456@gmail.com>
2018-11-02 00:52:19 +0800

    remove temp .cmake files

--- ethminer/utils/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alper/cpp-ethereum/utils
-# Build directory: /Users/alper/cpp-ethereum/ethminer/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libscrypt")
--- ethminer/utils/libscrypt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alper/cpp-ethereum/utils/libscrypt
-# Build directory: /Users/alper/cpp-ethereum/ethminer/utils/libscrypt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
